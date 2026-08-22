ba307bb107b4da2f864a2cb465af5cb60a1b6b80
Jiyaaa2006 <jiyavp30@gmail.com>
2026-08-22 11:09:37 +0530

    Update supabase-config.js

--- crime-reporting-site/js/supabase-config.js
+++ crime-reporting-site/js/supabase-config.js
@@ -1,7 +1,7 @@
 // ======================================================
 // STEP: Paste your own Supabase keys here (see README)
 // ======================================================
-const SUPABASE_URL = "https://aksccmuulrtwlmegizso.supabase.corest/v1/";
+const SUPABASE_URL = "https://aksccmuulrtwlmegizso.supabase.corest/v1";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrc2NjbXV1bHJ0d2xtZWdpenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNjk3MTEsImV4cCI6MjEwMjk0NTcxMX0.PSGCGJn_XX2bCXOYYteUSNuIEcGlbSlZYogUvl5EXoo";
 
 // This creates the connection to your database. Don't touch below this line.

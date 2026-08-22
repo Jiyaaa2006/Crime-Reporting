6e56912c228f76336410dea430a164eacf11eb34
Jiyaaa2006 <jiyavp30@gmail.com>
2026-08-22 10:17:53 +0530

    Update supabase-config.js

--- crime-reporting-site/js/supabase-config.js
+++ crime-reporting-site/js/supabase-config.js
@@ -1,8 +1,8 @@
 // ======================================================
 // STEP: Paste your own Supabase keys here (see README)
 // ======================================================
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://aksccmuulrtwlmegizso.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrc2NjbXV1bHJ0d2xtZWdpenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNjk3MTEsImV4cCI6MjEwMjk0NTcxMX0.PSGCGJn_XX2bCXOYYteUSNuIEcGlbSlZYogUvl5EXoo";
 
 // This creates the connection to your database. Don't touch below this line.
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
